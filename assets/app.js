478f2f0e65a06b0596eeddc7c7dd38b31c7a6670
Thomas <stingerkiller34@gmail.com>
2022-06-27 16:52:59 +0200

    correction install et debut react

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,11 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../styles/app.css';
+import Home from './components/Home';
+    
+ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
