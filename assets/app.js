2c67186f7435897dca0084fe728837402b531c55
Thomasr <thomas.abecassis@orange.fr>
2022-06-28 15:03:03 +0200

    installation windows + correction erreur + avancement react

--- assets/app.js
+++ assets/app.js
@@ -14,7 +14,6 @@ import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import '../styles/app.css';
 import Home from './components/Home';
     
 ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
